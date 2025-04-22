6d8be915e201739cd47cfadc666a5fb30283d228
Tiziano Pirez <tizippuden@gmail.com>
2025-04-22 19:10:30 -0300

    Create Ejercicio_1.cpp

---
+++ Laboratorio  2/Laboratorio 3/Ejercicio_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main {
+    int Numeros [10];
+    int Suma = 0;
+    for ( int i=0; i<10; i++) {
+        cout << "Escriba EL numero: " << i;
+        cin >> Numeros[i];
+        Suma = Suma + Numeros [i];
+        }
+float MediaAritmerica = suma/10;
+cout << "La suma de los numeros es: " << Suma << endl;
+cout << "La media aritmerica es: " << MediaAritmerica << endl;
+return 0;
+}
