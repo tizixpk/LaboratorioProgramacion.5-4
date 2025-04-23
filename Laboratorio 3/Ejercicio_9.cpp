2cb6a057ba6ce94c9255c6bede87eabd37999746
Tiziano Pirez <tizippuden@gmail.com>
2025-04-23 19:14:57 -0300

    Update Ejercicio_9.cpp

--- Laboratorio 3/Ejercicio_9.cpp
+++ Laboratorio 3/Ejercicio_9.cpp
@@ -1,24 +1,19 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    int vector[20], mayor, indice;
-
-    cout << "Ingrese 20 numeros:\n";
+    int vector[20], mayor = 0, indice = 0;
     for (int i = 0; i < 20; i++) {
+        cout << "Ingrese 20 numeros: ";
         cin >> vector[i];
-        if (i == 0 || vector[i] > mayor) {
+        if (i == 0 and vector[i] > mayor) {
             mayor = vector[i];
             indice = i;
         }
     }
-
     cout << "El mayor es: " << mayor << " en la posicion " << indice << endl;
-
-    cout << "Vector completo:\n";
+    cout << "Vector completo: ";
     for (int i = 0; i < 20; i++) {
         cout << vector[i] << " ";
     }
-
     return 0;
 }
