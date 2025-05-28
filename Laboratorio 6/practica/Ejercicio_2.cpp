da0a9f72afb3d8c9e2800d496d2dc181fec6a73e
Tiziano Pirez <tizippuden@gmail.com>
2025-05-28 16:47:46 -0300

    Create Ejercicio_2.cpp

---
+++ Laboratorio 6/practica/Ejercicio_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void Ejercicio_2() {
+    int vector[4] = {10, 20, 30, 40}; 
+    cout << "Contenido del vector: ";
+    for (int i = 0; i < 4; i++) {
+        cout << vector[i] << " ";
+    }
+    cout << endl;
+}
+int main() {
+    Ejercicio_2();
+    return 0;
+}
