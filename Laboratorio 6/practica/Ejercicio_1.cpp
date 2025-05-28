d3e55cd45a42776758fe84066b9381ad290a6bb8
Tiziano Pirez <tizippuden@gmail.com>
2025-05-28 16:44:30 -0300

    Create Ejercicio_1.cpp

---
+++ Laboratorio 6/practica/Ejercicio_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std;
+int a, b, r;
+int Ejercicio_1(int , int ){
+return r = a > b ? a : b;
+}
+int main() {
+cout << "ingresa los digitos" << endl;
+	cin >> a;
+	cin >> b;
+cout << "Mayor: "  << Ejercicio_1(a , b) << endl;
+return 0;
+}
