0e3149ab8151af2cf7c5d657a2c62898652a8348
Tiziano Pirez <tizippuden@gmail.com>
2025-06-07 12:47:30 -0300

    Update Ejercicio_1.cpp

--- Laboratorio 5/Ejercicio_1.cpp
+++ Laboratorio 5/Ejercicio_1.cpp
@@ -5,123 +5,117 @@ using namespace std;
 const int F = 4;
 const int C = 4;
 
-// esta función sirve  para imprimir la matriz
-void imprimirMatriz(int matriz[F][C]) {
-    for (int i = 0; i < F; i++) {
-        for (int j = 0; j < C; j++) {
-            cout << matriz[i][j] << "\t";
-        }
-        cout << endl;
+// Función para imprimir la matriz
+void imprimirMatriz(int m[F][C]) {
+    for (int i = 0; i < F; i++, cout << endl)
+        for (int j = 0; j < C; j++)
+            cout << m[i][j] << "\t";
+}
+
+// Función para llenar la matriz con números aleatorios
+void llenarMatriz(int m[F][C]) {
+    for (int i = 0; i < F; i++)
+        for (int j = 0; j < C; j++)
+            m[i][j] = rand() % 99 + 1;
+}
+
+// Promedio general
+float calcularPromedio(int m[F][C]) {
+    int suma = 0;
+    for (int i = 0; i < F; i++)
+        for (int j = 0; j < C; j++)
+            suma += m[i][j];
+    return (float)suma / (F * C);
+}
+
+// Suma de pares por columna
+void sumaParesPorColumna(int m[F][C]) {
+    for (int j = 0; j < C; j++) {
+        int suma = 0;
+        for (int i = 0; i < F; i++)
+            suma += (m[i][j] % 2 == 0) ? m[i][j] : 0;  // operador ternario
+        cout << "Columna " << j << ": " << suma << endl;
     }
+}
+
+// Suma de impares por fila
+void sumaImparesPorFila(int m[F][C]) {
+    for (int i = 0; i < F; i++) {
+        int suma = 0;
+        for (int j = 0; j < C; j++)
+            suma += (m[i][j] % 2 != 0) ? m[i][j] : 0;
+        cout << "Fila " << i << ": " << suma << endl;
+    }
+}
+
+// Buscar posición del máximo o mínimo (según el parámetro)
+void buscarValor(int m[F][C], bool buscarMax) {
+    int val = m[0][0], fila = 0, col = 0;
+    for (int i = 0; i < F; i++)
+        for (int j = 0; j < C; j++)
+            if ((buscarMax && m[i][j] > val) || (!buscarMax && m[i][j] < val)) {
+                val = m[i][j];
+                fila = i;
+                col = j;
+            }
+    cout << (buscarMax ? "Máximo" : "Mínimo") << ": " << val << " en (" << fila << ", " << col << ")\n";
+}
+
+// Ordenar fila ascendente
+void ordenarFilaAsc(int m[F][C], int fila) {
+    for (int i = 0; i < C - 1; i++)
+        for (int j = i + 1; j < C; j++)
+            if (m[fila][i] > m[fila][j])
+                swap(m[fila][i], m[fila][j]);
+}
+
+// Ordenar columna descendente
+void ordenarColDesc(int m[F][C], int col) {
+    for (int i = 0; i < F - 1; i++)
+        for (int j = i + 1; j < F; j++)
+            if (m[i][col] < m[j][col])
+                swap(m[i][col], m[j][col]);
+}
+
+// Intercambiar columnas
+void intercambiarColumnas(int m[F][C], int col1, int col2) {
+    for (int i = 0; i < F; i++)
+        swap(m[i][col1], m[i][col2]);
 }
 
 int main() {
     srand(time(0));
     int matriz[F][C];
 
-    // aca lleno la matriz con números aleatorios del 1 al 99
-    for (int i = 0; i < F; i++) {
-        for (int j = 0; j < C; j++) {
-            matriz[i][j] = rand() % 99 + 1;
-        }
-    }
-
+    llenarMatriz(matriz);
     cout << "Matriz original:\n";
     imprimirMatriz(matriz);
 
-    // promedio general de la matriz
-    int sumaTotal = 0;
-    for (int i = 0; i < F; i++)
-        for (int j = 0; j < C; j++)
-            sumaTotal += matriz[i][j];
+    cout << "\n// Promedio general de la matriz\n";
+    cout << "Promedio: " << calcularPromedio(matriz) << endl;
 
-    float promedio = (float)sumaTotal / (F * C);
-    cout << "\n// Promedio general de la matriz\n"; // el /n es para que quede bien cuando se ejecuta x eso lo pongo
-    cout << "Promedio: " << promedio << endl;
-
-    // suma de pares por columna
     cout << "\n// Suma de pares por columna\n";
-    for (int j = 0; j < C; j++) {
-        int sumaPares = 0;
-        for (int i = 0; i < F; i++) {
-            if (matriz[i][j] % 2 == 0)
-                sumaPares += matriz[i][j];
-        }
-        cout << "Columna " << j << ": " << sumaPares << endl;
-    }
+    sumaParesPorColumna(matriz);
 
-    // suma de impares por fila
     cout << "\n// Suma de impares por fila\n";
-    for (int i = 0; i < F; i++) {
-        int sumaImpares = 0;
-        for (int j = 0; j < C; j++) {
-            if (matriz[i][j] % 2 != 0)
-                sumaImpares += matriz[i][j];
-        }
-        cout << "Fila " << i << ": " << sumaImpares << endl;
-    }
+    sumaImparesPorFila(matriz);
 
-    // posición del máximo
-    int maxVal = matriz[0][0], maxFila = 0, maxCol = 0;
-    for (int i = 0; i < F; i++) {
-        for (int j = 0; j < C; j++) {
-            if (matriz[i][j] > maxVal) {
-                maxVal = matriz[i][j];
-                maxFila = i;
-                maxCol = j;
-            }
-        }
-    }
     cout << "\n// Posición del valor máximo\n";
-    cout << "Máximo: " << maxVal << " en (" << maxFila << ", " << maxCol << ")\n";
+    buscarValor(matriz, true);
 
-    // posición del mínimo
-    int minVal = matriz[0][0], minFila = 0, minCol = 0;
-    for (int i = 0; i < F; i++) {
-        for (int j = 0; j < C; j++) {
-            if (matriz[i][j] < minVal) {
-                minVal = matriz[i][j];
-                minFila = i;
-                minCol = j;
-            }
-        }
-    }
     cout << "\n// Posición del valor mínimo\n";
-    cout << "Mínimo: " << minVal << " en (" << minFila << ", " << minCol << ")\n";
+    buscarValor(matriz, false);
 
-    // ordenar fila 2 en forma ascendente (índice 2)
     cout << "\n// Ordenar en forma ascendente la fila 2\n";
-    for (int i = 0; i < C - 1; i++) {
-        for (int j = i + 1; j < C; j++) {
-            if (matriz[2][i] > matriz[2][j]) {
-                int temp = matriz[2][i];
-                matriz[2][i] = matriz[2][j];
-                matriz[2][j] = temp;
-            }
-        }
-    }
+    ordenarFilaAsc(matriz, 2);
     imprimirMatriz(matriz);
 
-    // ordenar columna 3 en forma descendente (índice 3)
     cout << "\n// Ordenar en forma descendente la columna 3\n";
-    for (int i = 0; i < F - 1; i++) {
-        for (int j = i + 1; j < F; j++) {
-            if (matriz[i][3] < matriz[j][3]) {
-                int temp = matriz[i][3];
-                matriz[i][3] = matriz[j][3];
-                matriz[j][3] = temp;
-            }
-        }
-    }
+    ordenarColDesc(matriz, 3);
     imprimirMatriz(matriz);
 
-    // intercambiar la columna 2 y 4 (índices 1 y 3)
     cout << "\n// Intercambiar la columna 2 y 4\n";
-    for (int i = 0; i < F; i++) {
-        int temp = matriz[i][1];   // la varible temp es para manipular la matriz  y imtercambiar las columnas
-        matriz[i][1] = matriz[i][3];
-        matriz[i][3] = temp;
-    }
+    intercambiarColumnas(matriz, 1, 3);
     imprimirMatriz(matriz);
 
     return 0;
